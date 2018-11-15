a3904102806967e683f0be901a84112877e4ed46
dylant44 <dt303271@students.davenport.k12.ia.us>
2018-11-15 12:51:33 -0600

    Tab Practice and Debugging

---
+++ TabPracticeGUI/TabPracticeGUI/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace TabPracticeGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.lblTab1 = new System.Windows.Forms.Label();
+            this.lblTab2 = new System.Windows.Forms.Label();
+            this.lblTab3 = new System.Windows.Forms.Label();
+            this.lblTab4 = new System.Windows.Forms.Label();
+            this.lblTab5 = new System.Windows.Forms.Label();
+            this.lblTab6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(243, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(243, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(243, 197);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(243, 247);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(243, 296);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(243, 344);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // lblTab1
+            // 
+            this.lblTab1.AutoSize = true;
+            this.lblTab1.Location = new System.Drawing.Point(153, 80);
+            this.lblTab1.Name = "lblTab1";
+            this.lblTab1.Size = new System.Drawing.Size(32, 13);
+            this.lblTab1.TabIndex = 6;
+            this.lblTab1.Text = "Tab1";
+            // 
+            // lblTab2
+            // 
+            this.lblTab2.AutoSize = true;
+            this.lblTab2.Location = new System.Drawing.Point(153, 140);
+            this.lblTab2.Name = "lblTab2";
+            this.lblTab2.Size = new System.Drawing.Size(35, 13);
+            this.lblTab2.TabIndex = 7;
+            this.lblTab2.Text = "Tab 2";
+            // 
+            // lblTab3
+            // 
+            this.lblTab3.AutoSize = true;
+            this.lblTab3.Location = new System.Drawing.Point(153, 200);
+            this.lblTab3.Name = "lblTab3";
+            this.lblTab3.Size = new System.Drawing.Size(35, 13);
+            this.lblTab3.TabIndex = 8;
+            this.lblTab3.Text = "Tab 3";
+            // 
+            // lblTab4
+            // 
+            this.lblTab4.AutoSize = true;
+            this.lblTab4.Location = new System.Drawing.Point(153, 250);
+            this.lblTab4.Name = "lblTab4";
+            this.lblTab4.Size = new System.Drawing.Size(32, 13);
+            this.lblTab4.TabIndex = 9;
+            this.lblTab4.Text = "Tab4";
+            // 
+            // lblTab5
+            // 
+            this.lblTab5.AutoSize = true;
+            this.lblTab5.Location = new System.Drawing.Point(153, 299);
+            this.lblTab5.Name = "lblTab5";
+            this.lblTab5.Size = new System.Drawing.Size(32, 13);
+            this.lblTab5.TabIndex = 10;
+            this.lblTab5.Text = "Tab5";
+            // 
+            // lblTab6
+            // 
+            this.lblTab6.AutoSize = true;
+            this.lblTab6.Location = new System.Drawing.Point(153, 347);
+            this.lblTab6.Name = "lblTab6";
+            this.lblTab6.Size = new System.Drawing.Size(35, 13);
+            this.lblTab6.TabIndex = 11;
+            this.lblTab6.Text = "Tab 6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTab6);
+            this.Controls.Add(this.lblTab5);
+            this.Controls.Add(this.lblTab4);
+            this.Controls.Add(this.lblTab3);
+            this.Controls.Add(this.lblTab2);
+            this.Controls.Add(this.lblTab1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label lblTab1;
+        private System.Windows.Forms.Label lblTab2;
+        private System.Windows.Forms.Label lblTab3;
+        private System.Windows.Forms.Label lblTab4;
+        private System.Windows.Forms.Label lblTab5;
+        private System.Windows.Forms.Label lblTab6;
+    }
+}
+
